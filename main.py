ae050dc1abde00307409d0fab54243c06efc0c32
Riki Satya Graha <riki.graha@gmail.com>
2024-09-30 09:32:11 +0700

    add comment start to learning python

--- main.py
+++ main.py
@@ -4,4 +4,5 @@ Ini adalah demo project pertama dengan Python
 print("Hello World")
 print("My Name is Riki Satya Graha")
 print("Start to Learning Python")
+print("First Time Learning Python with PyCharm")
 
